d6479096f7d58708ccb65b5d75c094820f3861db
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-13 05:13:40 -0500

    Feat(models/user.py): pycodestyle was corrected

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from os import getenv
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if (getenv('HBNB_TYPE_STORAGE') == 'db'):
