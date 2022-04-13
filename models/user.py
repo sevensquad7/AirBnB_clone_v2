0bc4a49466f6fac2d00f6cad4a4ae84fac32eceb
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-13 13:09:58 -0500

    Feat(models/user.py): relationship was added

--- models/user.py
+++ models/user.py
@@ -2,14 +2,22 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     if (getenv('HBNB_TYPE_STORAGE') == 'db'):
-        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        
