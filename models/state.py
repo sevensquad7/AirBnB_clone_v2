c5683fe3460ed292412b28cc5ac237de2fa9abcd
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-13 04:54:52 -0500

    Feat(models/state.py): New attributes was added and new conditional

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state')
+    else:
+        @property
+        def cities(self):
+            dict_city = storage.all(City)
+            store = list()
+            for key, value in dict_city.items():
+                if value.state_id == self.id:
+                    store.append(value)
+            return store
