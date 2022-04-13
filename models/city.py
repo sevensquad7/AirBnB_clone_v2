9e82bad94fb44c23e15e06a0d3c890575e5f432a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-13 13:09:51 -0500

    Feat(models/city.py): relationship was added

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -11,3 +12,7 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='cities')
+    else:
+        state_id = ""
+        name = ""
