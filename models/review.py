12e77ba21f0e0963dacff30b3bf1b5c256aefd68
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-13 14:18:23 -0500

    Feat(models/review.py): code was updated

--- models/review.py
+++ models/review.py
@@ -9,8 +9,8 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
     else:
         place_id = ""
