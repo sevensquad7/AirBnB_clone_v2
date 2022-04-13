424a38c3dce7680159a1083e57c3589e04564227
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-13 04:56:31 -0500

    Add: models/engine/db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" New Engine """
+from sqlalchemy import create_engine
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}"
+            .format(getenv('HBNB_MYSQL_USER'),
+                    getenv('HBNB_MYSQL_PWD'),
+                    getenv('HBNB_MYSQL_HOST'),
+                    getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        if cls is None:
+            return DBStorage.__session
+        db = self.__session.query(cls)
+        store = {}
+        for obj in db:
+            store[obj.to_dict()['__class__'] + '.' + obj.id] = obj
+        return store
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker()
+        Session.configure(bind=self.__engine, expire_on_commit=False)
+        self.__session = Session()
