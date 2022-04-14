5141428c2cb06fcb4c6a3d5e121912f7c778c390
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-13 23:38:33 -0500

    Feat(models/amenity.py): comment was removed

--- models/amenity.py
+++ models/amenity.py
@@ -10,7 +10,7 @@ class Amenity(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-#        place_amenities = relationship('Place', secondary='place_amenity',
-#                                       back_populates='amenities')
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates='amenities')
     else:
         name = ""
