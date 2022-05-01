98a9fb01e13ef9aee4fc06b3f0f75478f4d8f863
zye7ert <edgarxot@gmail.com>
2022-05-01 12:59:36 -0500

    load file Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,6 +17,6 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 ## Service nginx - start.
 sudo service nginx start
 ## adding line into location /etc/nginx/sites-available/default
-sudo sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 ## Restart service nginx
 sudo service nginx restart
