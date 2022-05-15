ebc02cb9ab71d84338f854122daa8299790cc9ec
zye7ert <edgarxot@gmail.com>
2022-05-14 21:56:09 -0500

    load file Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(debug="true",host="0.0.0.0", port=5000)
