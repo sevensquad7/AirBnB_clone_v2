92637f54f4d09ca8b3fa2b28ff37ba6c93c427e0
zye7ert <edgarxot@gmail.com>
2022-05-14 22:07:41 -0500

    load file Hello Flask! added comentary

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask
-"""
-Write a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-"""
+"""Write a script that starts a Flask web application:"""
 
 app = Flask(__name__)
 
