42616f5a464b7e20fa81dfc446f9d2eed89c97d5
zye7ert <edgarxot@gmail.com>
2022-05-14 21:59:55 -0500

    load file Hello Flask! added comentary

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 from flask import Flask
+"""
+Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """Return Hello HBNB"""
     return "Hello HBNB!"
 
 
